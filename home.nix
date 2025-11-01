5da23b6b188dce37bb218c229126f41d59682cee
jeremy <misolarellinj06@gmail.com>
2025-11-01 19:49:09 -0400

    initial fix home

--- home.nix
+++ home.nix
@@ -1,29 +1,14 @@
-{ config, pkgs, ...}:
+{ config, pkgs, ... }:
 
 {
-  imports = [
-   ];
-    
-  # The User and Path it manages
   home.username = "jurassicjey";
   home.homeDirectory = "/home/jurassicjey";
-  
-  programs.home-manager.enable = true;
-  
-  home.packages = with pkgs; [
-    zsh
-  ];
-  
-  # ZSH
-  programs.zsh = {
+  programs.git.enable = true;
+  home.stateVersion = "25.05";
+  programs.bash = {
     enable = true;
-    enableCompletion = true;
-    enableAutosuggestions = true
     shellAliases = {
-      _ = "sudo";
-      h = "history";
-      hg = "history | grep ";
+      btw = "echo i use nixos, btw";
     };
   };
-  
 }
